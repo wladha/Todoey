5c880e81b1f03e8e06e3d37c9a4efc0e8bc36e7f
Waheed Ladha <waheed@waheeds-macbook-pro.local>
2019-03-15 20:19:46 +0000

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Waheed Ladha on 15/03/2019.
+//  Copyright © 2019 Soultech. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
